a3778fd7677b4c829877dd986e132898df895815
Pyry Haulos <pyry.haulos@gmail.com>
2007-10-28 10:19:51 +0200

    Kill whitespaces in info.

--- src/stream/info.c
+++ src/stream/info.c
@@ -42,10 +42,10 @@ struct info_ctx_s {
 	unsigned int w, h;
 	glc_ctx_i ctx_i;
 	unsigned long pictures;
-	
+
 	unsigned long fps;
 	glc_utime_t last_fps_time, fps_time;
-	
+
 	struct info_ctx_s *next;
 };
 
@@ -74,35 +74,35 @@ int info_init(glc_t *glc, ps_buffer_t *from)
 {
 	struct info_private_s *info = malloc(sizeof(struct info_private_s));
 	memset(info, 0, sizeof(struct info_private_s));
-	
+
 	info->glc = glc;
 	info->ctx_list = NULL;
 	info->time = 0;
-	
+
 	info->thread.flags = GLC_THREAD_READ;
 	info->thread.ptr = info;
 	info->thread.read_callback = &info_read_callback;
 	info->thread.finish_callback = &info_finish_callback;
 	info->thread.threads = 1;
-	
+
 	return glc_thread_create(glc, &info->thread, from, NULL);
 }
 
 void info_finish_callback(void *ptr, int err)
 {
 	struct info_private_s *info = (struct info_private_s *) ptr;
-	
+
 	if (err)
 		fprintf(stderr, "info failed: %s (%d)\n", strerror(err), err);
-	
+
 	struct info_ctx_s *del;
 	while (info->ctx_list != NULL) {
 		del = info->ctx_list;
 		info->ctx_list = info->ctx_list->next;
-		
+
 		free(del);
 	}
-	
+
 	sem_post(&info->glc->signal[GLC_SIGNAL_INFO_FINISHED]);
 	free(info);
 }
@@ -110,10 +110,10 @@ void info_finish_callback(void *ptr, int err)
 int info_read_callback(glc_thread_state_t *state)
 {
 	struct info_private_s *info = (struct info_private_s *) state->ptr;
-	
+
 	if (!info->stream_info)
 		stream_info(info);
-	
+
 	if (state->header.type == GLC_MESSAGE_CTX)
 		ctx_info(info, (glc_ctx_message_t *) state->read_data);
 	else if (state->header.type == GLC_MESSAGE_PICTURE)
@@ -129,31 +129,31 @@ int info_read_callback(glc_thread_state_t *state)
 		print_time(stdout, info->time);
 		printf("error: unknown %zd B message with type 0x%02x\n", state->read_size, state->header.type);
 	}
-	
+
 	return 0;
 }
 
 int info_get_ctx(struct info_private_s *info, struct info_ctx_s **ctx, glc_ctx_i ctx_i)
 {
 	struct info_ctx_s *fctx = info->ctx_list;
-	
+
 	while (fctx != NULL) {
 		if (fctx->ctx_i == ctx_i)
 			break;
 		fctx = fctx->next;
 	}
-	
+
 	if (fctx == NULL) {
 		fctx = (struct info_ctx_s *) malloc(sizeof(struct info_ctx_s));
 		memset(fctx, 0, sizeof(struct info_ctx_s));
-		
+
 		fctx->next = info->ctx_list;
 		info->ctx_list = fctx;
 		fctx->ctx_i = ctx_i;
 	}
-	
+
 	*ctx = fctx;
-	
+
 	return 0;
 }
 
@@ -171,27 +171,27 @@ void ctx_info(struct info_private_s *info, glc_ctx_message_t *ctx_message)
 	INFO_FLAGS
 	struct info_ctx_s *ctx;
 	info_get_ctx(info, &ctx, ctx_message->ctx);
-	
+
 	ctx->w = ctx_message->w;
 	ctx->h = ctx_message->h;
-	
+
 	if ((ctx_message->flags & GLC_CTX_UPDATE) && (!(ctx->created))) {
 		print_time(stdout, info->time);
 		printf("error: GLC_CTX_UPDATE to uninitialized ctx %d\n", ctx_message->ctx);
 	}
-	
+
 	if ((ctx_message->flags & GLC_CTX_CREATE) && (ctx->created)) {
 		print_time(stdout, info->time);
 		printf("error: GLC_CTX_CREATE to initalized ctx %d\n", ctx_message->ctx);
 	}
-	
+
 	if (ctx_message->flags & GLC_CTX_CREATE)
 		ctx->created = 1;
-	
+
 	print_time(stdout, info->time);
 	if (info->glc->info_level >= INFO_DETAILED_CTX) {
 		printf("ctx message\n");
-		
+
 		printf("  ctx         = %d\n", ctx_message->ctx);
 		printf("  flags       = ");
 		INFO_FLAG(ctx_message->flags, GLC_CTX_CREATE)
@@ -222,7 +222,7 @@ void pic_info(struct info_private_s *info, glc_picture_header_t *pic_header)
 	if (info->glc->info_level >= INFO_DETAILED_PICTURE) {
 		print_time(stdout, info->time);
 		printf("picture\n");
-		
+
 		printf("  timestamp   = %lu\n", pic_header->timestamp);
 		printf("  ctx         = %d\n", pic_header->ctx);
 		printf("  size        = %ux%u\n", ctx->w, ctx->h);
@@ -274,7 +274,7 @@ void audio_fmt_info(struct info_private_s *info, glc_audio_format_message_t *fmt
 void audio_info(struct info_private_s *info, glc_audio_header_t *audio_header)
 {
 	info->time = audio_header->timestamp;
-	
+
 	if (info->glc->info_level >= INFO_AUDIO_DETAILED) {
 		print_time(stdout, info->time);
 		printf("audio packet\n");
@@ -291,7 +291,7 @@ void stream_info(struct info_private_s *info)
 {
 	if (info->stream_info)
 		return;
-	
+
 	/* show stream info header */
 	if (info->glc->info) {
 		printf("GLC stream info\n");
@@ -304,7 +304,7 @@ void stream_info(struct info_private_s *info)
 		printf("  date        = %s\n", info->glc->info_date);
 	} else
 		printf("no GLC stream info available\n");
-	
+
 	info->stream_info = 1;
 }
 
