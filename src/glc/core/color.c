7558c8d57cda64d0034b8ef2e71f64be4df7123b
Pyry Haulos <pyry.haulos@gmail.com>
2008-03-18 22:17:46 +0200

    Use new format and types in core/color

--- src/glc/core/color.c
+++ src/glc/core/color.c
@@ -37,14 +37,14 @@
 #define COLOR_RUNNING     0x1
 #define COLOR_OVERRIDE    0x2
 
-struct color_ctx_s;
+struct color_video_stream_s;
 
-typedef void (*color_proc)(color_t color, struct color_ctx_s *ctx,
+typedef void (*color_proc)(color_t color, struct color_video_stream_s *video,
 			   unsigned char *from, unsigned char *to);
 
-struct color_ctx_s {
-	glc_ctx_i ctx_i;
-	glc_flags_t flags;
+struct color_video_stream_s {
+	glc_stream_id_t id;
+	glc_video_format_t format;
 	unsigned int w, h;
 
 	unsigned int bpp, row;
@@ -56,7 +56,7 @@ struct color_ctx_s {
 	color_proc proc;
 
 	pthread_rwlock_t update;
-	struct color_ctx_s *next;
+	struct color_video_stream_s *next;
 };
 
 struct color_s {
@@ -64,7 +64,7 @@ struct color_s {
 	glc_flags_t flags;
 	glc_thread_t thread;
 
-	struct color_ctx_s *ctx;
+	struct color_video_stream_s *video;
 
 	float brightness, contrast;
 	float red_gamma, green_gamma, blue_gamma;
@@ -74,20 +74,20 @@ int color_read_callback(glc_thread_state_t *state);
 int color_write_callback(glc_thread_state_t *state);
 void color_finish_callback(void *ptr, int err);
 
-void color_get_ctx(color_t color, glc_ctx_i ctx_i,
-		   struct color_ctx_s **ctx);
+void color_get_video_stream(color_t color, glc_stream_id_t id,
+		   struct color_video_stream_s **video);
 
-int color_ctx_msg(color_t color, glc_ctx_message_t *msg);
+int color_video_msg(color_t color, glc_video_format_message_t *msg);
 int color_color_msg(color_t color, glc_color_message_t *msg);
 
 int color_generate_ycbcr_lookup_table(color_t color,
-				      struct color_ctx_s *ctx);
+				      struct color_video_stream_s *video);
 int color_generate_rgb_lookup_table(color_t color,
-				    struct color_ctx_s *ctx);
+				    struct color_video_stream_s *video);
 
-void color_ycbcr(color_t color, struct color_ctx_s *ctx,
+void color_ycbcr(color_t color, struct color_video_stream_s *video,
 		 unsigned char *from, unsigned char *to);
-void color_bgr(color_t color, struct color_ctx_s *ctx,
+void color_bgr(color_t color, struct color_video_stream_s *video,
 	       unsigned char *from, unsigned char *to);
 
 /* unfortunately over- and underflows will occur */
@@ -169,14 +169,14 @@ int color_override_clear(color_t color)
 void color_finish_callback(void *ptr, int err)
 {
 	color_t color = (color_t) ptr;
-	struct color_ctx_s *del;
+	struct color_video_stream_s *del;
 
 	if (err)
 		glc_log(color->glc, GLC_ERROR, "color", "%s (%d)", strerror(err), err);
 
-	while (color->ctx != NULL) {
-		del = color->ctx;
-		color->ctx = color->ctx->next;
+	while (color->video != NULL) {
+		del = color->video;
+		color->video = color->video->next;
 
 		pthread_rwlock_destroy(&del->update);
 		if (del->lookup_table)
@@ -188,8 +188,8 @@ void color_finish_callback(void *ptr, int err)
 int color_read_callback(glc_thread_state_t *state)
 {
 	color_t color = (color_t) state->ptr;
-	struct color_ctx_s *ctx;
-	glc_picture_header_t *pic_hdr;
+	struct color_video_stream_s *video;
+	glc_video_data_header_t *pic_hdr;
 
 	if (state->header.type == GLC_MESSAGE_COLOR) {
 		color_color_msg(color, (glc_color_message_t *) state->read_data);
@@ -199,19 +199,19 @@ int color_read_callback(glc_thread_state_t *state)
 		return 0;
 	}
 
-	if (state->header.type == GLC_MESSAGE_CTX)
-		color_ctx_msg(color, (glc_ctx_message_t *) state->read_data);
+	if (state->header.type == GLC_MESSAGE_VIDEO_FORMAT)
+		color_video_msg(color, (glc_video_format_message_t *) state->read_data);
 
-	if (state->header.type == GLC_MESSAGE_PICTURE) {
-		pic_hdr = (glc_picture_header_t *) state->read_data;
-		color_get_ctx(color, pic_hdr->ctx, &ctx);
-		state->threadptr = ctx;
+	if (state->header.type == GLC_MESSAGE_VIDEO_DATA) {
+		pic_hdr = (glc_video_data_header_t *) state->read_data;
+		color_get_video_stream(color, pic_hdr->id, &video);
+		state->threadptr = video;
 
-		pthread_rwlock_rdlock(&ctx->update);
+		pthread_rwlock_rdlock(&video->update);
 
-		if (ctx->proc == NULL) {
+		if (video->proc == NULL) {
 			state->flags |= GLC_THREAD_COPY;
-			pthread_rwlock_unlock(&ctx->update);
+			pthread_rwlock_unlock(&video->update);
 		}
 	} else
 		state->flags |= GLC_THREAD_COPY;
@@ -221,159 +221,164 @@ int color_read_callback(glc_thread_state_t *state)
 
 int color_write_callback(glc_thread_state_t *state)
 {
-	struct color_ctx_s *ctx = state->threadptr;
+	struct color_video_stream_s *video = state->threadptr;
 
-	memcpy(state->write_data, state->read_data, GLC_PICTURE_HEADER_SIZE);
-	ctx->proc(state->ptr, ctx,
-		  (unsigned char *) &state->read_data[GLC_PICTURE_HEADER_SIZE],
-		  (unsigned char *) &state->write_data[GLC_PICTURE_HEADER_SIZE]);
+	memcpy(state->write_data, state->read_data, GLC_VIDEO_DATA_HEADER_SIZE);
+	video->proc(state->ptr, video,
+		  (unsigned char *) &state->read_data[GLC_VIDEO_DATA_HEADER_SIZE],
+		  (unsigned char *) &state->write_data[GLC_VIDEO_DATA_HEADER_SIZE]);
 
-	pthread_rwlock_unlock(&ctx->update);
+	pthread_rwlock_unlock(&video->update);
 	return 0;
 }
 
-void color_get_ctx(color_t color, glc_ctx_i ctx_i,
-		   struct color_ctx_s **ctx)
+void color_get_video_stream(color_t color, glc_stream_id_t id,
+		   struct color_video_stream_s **video)
 {
 	/* this function is called from read callback so it is never
 	   called in parallel */
-	*ctx = color->ctx;
+	*video = color->video;
 
-	while (*ctx != NULL) {
-		if ((*ctx)->ctx_i == ctx_i)
+	while (*video != NULL) {
+		if ((*video)->id == id)
 			break;
-		*ctx = (*ctx)->next;
+		*video = (*video)->next;
 	}
 
-	if (*ctx == NULL) {
-		*ctx = malloc(sizeof(struct color_ctx_s));
-		memset(*ctx, 0, sizeof(struct color_ctx_s));
+	if (*video == NULL) {
+		*video = malloc(sizeof(struct color_video_stream_s));
+		memset(*video, 0, sizeof(struct color_video_stream_s));
 
-		(*ctx)->next = color->ctx;
-		color->ctx = *ctx;
-		(*ctx)->ctx_i = ctx_i;
-		pthread_rwlock_init(&(*ctx)->update, NULL);
+		(*video)->next = color->video;
+		color->video = *video;
+		(*video)->id = id;
+		pthread_rwlock_init(&(*video)->update, NULL);
 	}
 }
 
-int color_ctx_msg(color_t color, glc_ctx_message_t *msg)
+int color_video_format_msg(color_t color, glc_video_format_message_t *msg)
 {
-	struct color_ctx_s *ctx;
-	glc_flags_t old_flags;
+	struct color_video_stream_s *video;
+	glc_video_format_t old_format;
 
-	color_get_ctx(color, msg->ctx, &ctx);
-	pthread_rwlock_wrlock(&ctx->update);
+	color_get_video_stream(color, msg->id, &video);
+	pthread_rwlock_wrlock(&video->update);
 
-	old_flags = ctx->flags;
-	ctx->flags = msg->flags;
-	ctx->w = msg->w;
-	ctx->h = msg->h;
+	old_format = video->format;
+	video->format = msg->format;
+	video->w = msg->width;
+	video->h = msg->height;
 
-	if ((ctx->flags & GLC_CTX_BGR) | (ctx->flags & GLC_CTX_BGRA)) {
-		if (ctx->flags & GLC_CTX_BGRA)
-			ctx->bpp = 4;
+	if ((video->format == GLC_VIDEO_BGR) |
+	    (video->format == GLC_VIDEO_BGRA)) {
+		if (video->format == GLC_VIDEO_BGRA)
+			video->bpp = 4;
 		else
-			ctx->bpp = 3;
+			video->bpp = 3;
 
-		ctx->row = ctx->bpp * ctx->w;
+		video->row = video->bpp * video->w;
 
-		if ((ctx->flags & GLC_CTX_DWORD_ALIGNED) && (ctx->row % 8 != 0))
-			ctx->row += 8 - ctx->row % 8;
+		if ((msg->flags & GLC_VIDEO_DWORD_ALIGNED) &&
+		    (video->row % 8 != 0))
+			video->row += 8 - video->row % 8;
 	}
 
 	if (color->flags & COLOR_OVERRIDE) {
-		ctx->brightness = color->brightness;
-		ctx->contrast = color->contrast;
-		ctx->red_gamma = color->red_gamma;
-		ctx->green_gamma = color->green_gamma;
-		ctx->blue_gamma = color->blue_gamma;
+		video->brightness = color->brightness;
+		video->contrast = color->contrast;
+		video->red_gamma = color->red_gamma;
+		video->green_gamma = color->green_gamma;
+		video->blue_gamma = color->blue_gamma;
 
 		glc_log(color->glc, GLC_INFORMATION, "color",
-			 "using global color correction for ctx %d", msg->ctx);
+			 "using global color correction for video %d", msg->id);
 		glc_log(color->glc, GLC_INFORMATION, "color",
-			 "ctx %d: brightness=%f, contrast=%f, red=%f, green=%f, blue=%f",
-			 msg->ctx, ctx->brightness, ctx->contrast,
-			 ctx->red_gamma, ctx->green_gamma, ctx->blue_gamma);
+			 "video stream %d: brightness=%f, contrast=%f, red=%f, green=%f, blue=%f",
+			 msg->id, video->brightness, video->contrast,
+			 video->red_gamma, video->green_gamma, video->blue_gamma);
 
-		if ((ctx->brightness == 0) &&
-		    (ctx->contrast == 0) &&
-		    (ctx->red_gamma == 1) &&
-		    (ctx->green_gamma == 1) &&
-		    (ctx->blue_gamma == 1)) {
+		if ((video->brightness == 0) &&
+		    (video->contrast == 0) &&
+		    (video->red_gamma == 1) &&
+		    (video->green_gamma == 1) &&
+		    (video->blue_gamma == 1)) {
 			glc_log(color->glc, GLC_INFORMATION, "color", "skipping color correction");
-			ctx->proc = NULL;
-		} else if (ctx->flags & GLC_CTX_YCBCR_420JPEG) {
-			color_generate_ycbcr_lookup_table(color, ctx);
-			ctx->proc = &color_ycbcr;
-		} else if ((ctx->flags & GLC_CTX_BGR) | (ctx->flags & GLC_CTX_BGRA)) {
-			color_generate_rgb_lookup_table(color, ctx);
-			ctx->proc = &color_bgr;
+			video->proc = NULL;
+		} else if (video->format == GLC_VIDEO_YCBCR_420JPEG) {
+			color_generate_ycbcr_lookup_table(color, video);
+			video->proc = &color_ycbcr;
+		} else if ((video->format == GLC_VIDEO_BGR) | (video->format == GLC_VIDEO_BGRA)) {
+			color_generate_rgb_lookup_table(color, video);
+			video->proc = &color_bgr;
 		} else {
 			/* set proc NULL -> no conversion done */
-			glc_log(color->glc, GLC_WARNING, "color", "unsupported ctx %d", msg->ctx);
-			ctx->proc = NULL;
+			glc_log(color->glc, GLC_WARNING, "color", "unsupported video %d", msg->id);
+			video->proc = NULL;
 		}
-	} else if (((old_flags & GLC_CTX_BGR) | (old_flags & GLC_CTX_BGRA)) &&
-		   (msg->flags & GLC_CTX_YCBCR_420JPEG)) {
+	} else if (((old_format == GLC_VIDEO_BGR) |
+		    (old_format == GLC_VIDEO_BGRA)) &&
+		   (msg->format == GLC_VIDEO_YCBCR_420JPEG)) {
 		glc_log(color->glc, GLC_WARNING, "color",
 			 "colorspace switched from RGB to Y'CbCr, recalculating lookup table");
-		color_generate_ycbcr_lookup_table(color, ctx);
-		ctx->proc = &color_ycbcr;
-	} else if (((msg->flags & GLC_CTX_BGR) | (msg->flags & GLC_CTX_BGRA)) &&
-		 (old_flags & GLC_CTX_YCBCR_420JPEG)) {
+		color_generate_ycbcr_lookup_table(color, video);
+		video->proc = &color_ycbcr;
+	} else if (((msg->format == GLC_VIDEO_BGR) |
+		    (msg->format == GLC_VIDEO_BGRA)) &&
+		   (old_format == GLC_VIDEO_YCBCR_420JPEG)) {
 		glc_log(color->glc, GLC_WARNING, "color",
 			 "colorspace switched from Y'CbCr to RGB, recalculating lookup table");
-		color_generate_rgb_lookup_table(color, ctx);
-		ctx->proc = &color_bgr;
+		color_generate_rgb_lookup_table(color, video);
+		video->proc = &color_bgr;
 	}
 
-	pthread_rwlock_unlock(&ctx->update);
+	pthread_rwlock_unlock(&video->update);
 	return 0;
 }
 
 int color_color_msg(color_t color, glc_color_message_t *msg)
 {
-	struct color_ctx_s *ctx;
+	struct color_video_stream_s *video;
 
 	if (color->flags & COLOR_OVERRIDE)
 		return 0; /* ignore */
 
-	color_get_ctx(color, msg->ctx, &ctx);
-	pthread_rwlock_wrlock(&ctx->update);
+	color_get_video_stream(color, msg->id, &video);
+	pthread_rwlock_wrlock(&video->update);
 
-	ctx->brightness = msg->brightness;
-	ctx->contrast = msg->contrast;
-	ctx->red_gamma = msg->red;
-	ctx->green_gamma = msg->green;
-	ctx->blue_gamma = msg->blue;
+	video->brightness = msg->brightness;
+	video->contrast = msg->contrast;
+	video->red_gamma = msg->red;
+	video->green_gamma = msg->green;
+	video->blue_gamma = msg->blue;
 
 	glc_log(color->glc, GLC_INFORMATION, "color",
-		 "ctx %d: brightness=%f, contrast=%f, red=%f, green=%f, blue=%f",
-		 msg->ctx, ctx->brightness, ctx->contrast,
-		 ctx->red_gamma, ctx->green_gamma, ctx->blue_gamma);
+		 "video stream %d: brightness=%f, contrast=%f, red=%f, green=%f, blue=%f",
+		 msg->id, video->brightness, video->contrast,
+		 video->red_gamma, video->green_gamma, video->blue_gamma);
 
-	if ((ctx->brightness == 0) &&
-	    (ctx->contrast == 0) &&
-	    (ctx->red_gamma == 1) &&
-	    (ctx->green_gamma == 1) &&
-	    (ctx->blue_gamma == 1)) {
+	if ((video->brightness == 0) &&
+	    (video->contrast == 0) &&
+	    (video->red_gamma == 1) &&
+	    (video->green_gamma == 1) &&
+	    (video->blue_gamma == 1)) {
 		glc_log(color->glc, GLC_INFORMATION, "color", "skipping color correction");
-		ctx->proc = NULL;
-	} else if (ctx->flags & GLC_CTX_YCBCR_420JPEG) {
-		color_generate_ycbcr_lookup_table(color, ctx);
-		ctx->proc = &color_ycbcr;
-	} else if ((ctx->flags & GLC_CTX_BGR) | (ctx->flags & GLC_CTX_BGRA)) {
-		color_generate_rgb_lookup_table(color, ctx);
-		ctx->proc = &color_bgr;
+		video->proc = NULL;
+	} else if (video->format == GLC_VIDEO_YCBCR_420JPEG) {
+		color_generate_ycbcr_lookup_table(color, video);
+		video->proc = &color_ycbcr;
+	} else if ((video->format == GLC_VIDEO_BGR) |
+		   (video->format == GLC_VIDEO_BGRA)) {
+		color_generate_rgb_lookup_table(color, video);
+		video->proc = &color_bgr;
 	} else
-		ctx->proc = NULL; /* don't attempt anything... */
+		video->proc = NULL; /* don't attempt anything... */
 
-	pthread_rwlock_unlock(&ctx->update);
+	pthread_rwlock_unlock(&video->update);
 	return 0;
 }
 
 void color_ycbcr(color_t color,
-		 struct color_ctx_s *ctx,
+		 struct color_video_stream_s *video,
 		 unsigned char *from, unsigned char *to)
 {
 	unsigned int x, y, Cpix, Y;
@@ -382,23 +387,23 @@ void color_ycbcr(color_t color,
 	unsigned char *Y_to, *Cb_to, *Cr_to;
 
 	Y_from = from;
-	Cb_from = &from[ctx->h * ctx->w];
-	Cr_from = &from[ctx->h * ctx->w + (ctx->h / 2) * (ctx->w / 2)];
+	Cb_from = &from[video->h * video->w];
+	Cr_from = &from[video->h * video->w + (video->h / 2) * (video->w / 2)];
 
 	Y_to = to;
-	Cb_to = &to[ctx->h * ctx->w];
-	Cr_to = &to[ctx->h * ctx->w + (ctx->h / 2) * (ctx->w / 2)];
+	Cb_to = &to[video->h * video->w];
+	Cr_to = &to[video->h * video->w + (video->h / 2) * (video->w / 2)];
 
 	Cpix = 0;
 
 #define CONVERT_Y(xadd, yadd) 								\
-	pos = YCBCR_LOOKUP_POS(Y_from[(x + (xadd)) + (y + (yadd)) * ctx->w],		\
+	pos = YCBCR_LOOKUP_POS(Y_from[(x + (xadd)) + (y + (yadd)) * video->w],		\
 			       Cb_from[Cpix], Cr_from[Cpix]);				\
-	Y_to[(x + (xadd)) + (y + (yadd)) * ctx->w] = ctx->lookup_table[pos + 0];	\
-	Y += ctx->lookup_table[pos + 0];
+	Y_to[(x + (xadd)) + (y + (yadd)) * video->w] = video->lookup_table[pos + 0];	\
+	Y += video->lookup_table[pos + 0];
 
-	for (y = 0; y < ctx->h; y += 2) {
-		for (x = 0; x < ctx->w; x += 2) {
+	for (y = 0; y < video->h; y += 2) {
+		for (x = 0; x < video->w; x += 2) {
 			Y = 0;
 
 			CONVERT_Y(0, 0)
@@ -407,8 +412,8 @@ void color_ycbcr(color_t color,
 			CONVERT_Y(1, 1)
 
 			pos = YCBCR_LOOKUP_POS(Y >> 2, Cb_from[Cpix], Cr_from[Cpix]);
-			Cb_to[Cpix] = ctx->lookup_table[pos + 1];
-			Cr_to[Cpix] = ctx->lookup_table[pos + 2];
+			Cb_to[Cpix] = video->lookup_table[pos + 1];
+			Cr_to[Cpix] = video->lookup_table[pos + 2];
 
 			Cpix++;
 		}
@@ -416,18 +421,18 @@ void color_ycbcr(color_t color,
 }
 
 void color_bgr(color_t color,
-	       struct color_ctx_s *ctx,
+	       struct color_video_stream_s *video,
 	       unsigned char *from, unsigned char *to)
 {
 	unsigned int x, y, p;
 
-	for (y = 0; y < ctx->h; y++) {
-		for (x = 0; x < ctx->w; x++) {
-			p = ctx->row * y + x * ctx->bpp;
+	for (y = 0; y < video->h; y++) {
+		for (x = 0; x < video->w; x++) {
+			p = video->row * y + x * video->bpp;
 
-			to[p + 0] = ctx->lookup_table[256 + 256 + from[p + 0]];
-			to[p + 1] = ctx->lookup_table[256       + from[p + 1]];
-			to[p + 2] = ctx->lookup_table[            from[p + 2]];
+			to[p + 0] = video->lookup_table[256 + 256 + from[p + 0]];
+			to[p + 1] = video->lookup_table[256       + from[p + 1]];
+			to[p + 2] = video->lookup_table[            from[p + 2]];
 		}
 	}
 }
@@ -461,7 +466,7 @@ unsigned char YCbCr_TO_RGB_Bd(unsigned char Y, unsigned char Cb, unsigned char C
 	(128 + 0.5      * (Rd) - 0.418688 * (Gd) - 0.081312 * (Bd))
 
 int color_generate_ycbcr_lookup_table(color_t color,
-				      struct color_ctx_s *ctx)
+				      struct color_video_stream_s *video)
 {
 	unsigned int Y, Cb, Cr, pos;
 	unsigned char R, G, B;
@@ -469,7 +474,7 @@ int color_generate_ycbcr_lookup_table(color_t color,
 
 	glc_log(color->glc, GLC_INFORMATION, "color",
 		 "using %d bit lookup table (%zd bytes)", LOOKUP_BITS, lookup_size);
-	ctx->lookup_table = malloc(lookup_size);
+	video->lookup_table = malloc(lookup_size);
 
 #define CALC(value, brightness, contrast, gamma) \
 	((pow((double) value / 255.0, 1.0 / gamma) - 0.5) * (1.0 + contrast) \
@@ -480,15 +485,15 @@ int color_generate_ycbcr_lookup_table(color_t color,
 		for (Cb = 0; Cb < 256; Cb += (1 << (8 - LOOKUP_BITS))) {
 			for (Cr = 0; Cr < 256; Cr += (1 << (8 - LOOKUP_BITS))) {
 				R = color_clamp(CALC(YCbCr_TO_RGB_Rd(Y, Cb, Cr),
-						ctx->brightness, ctx->contrast, ctx->red_gamma));
+						video->brightness, video->contrast, video->red_gamma));
 				G = color_clamp(CALC(YCbCr_TO_RGB_Gd(Y, Cb, Cr),
-						ctx->brightness, ctx->contrast, ctx->green_gamma));
+						video->brightness, video->contrast, video->green_gamma));
 				B = color_clamp(CALC(YCbCr_TO_RGB_Bd(Y, Cb, Cr),
-						ctx->brightness, ctx->contrast, ctx->blue_gamma));
+						video->brightness, video->contrast, video->blue_gamma));
 
-				ctx->lookup_table[pos + 0] = RGB_TO_YCbCrJPEG_Y(R, G, B);
-				ctx->lookup_table[pos + 1] = RGB_TO_YCbCrJPEG_Cb(R, G, B);
-				ctx->lookup_table[pos + 2] = RGB_TO_YCbCrJPEG_Cr(R, G, B);
+				video->lookup_table[pos + 0] = RGB_TO_YCbCrJPEG_Y(R, G, B);
+				video->lookup_table[pos + 1] = RGB_TO_YCbCrJPEG_Cb(R, G, B);
+				video->lookup_table[pos + 2] = RGB_TO_YCbCrJPEG_Cr(R, G, B);
 				pos += 3;
 			}
 		}
@@ -500,11 +505,11 @@ int color_generate_ycbcr_lookup_table(color_t color,
 }
 
 int color_generate_rgb_lookup_table(color_t color,
-				    struct color_ctx_s *ctx)
+				    struct color_video_stream_s *video)
 {
 	unsigned int c;
 
-	ctx->lookup_table = malloc(256 + 256 + 256);
+	video->lookup_table = malloc(256 + 256 + 256);
 
 #define CALC(value, brightness, contrast, gamma) \
 	color_clamp( \
@@ -513,13 +518,13 @@ int color_generate_rgb_lookup_table(color_t color,
 		)
 
 	for (c = 0; c < 256; c++)
-		ctx->lookup_table[c + 0] = CALC(c, ctx->brightness, ctx->contrast, ctx->red_gamma);
+		video->lookup_table[c + 0] = CALC(c, video->brightness, video->contrast, video->red_gamma);
 
 	for (c = 0; c < 256; c++)
-		ctx->lookup_table[c + 256] = CALC(c, ctx->brightness, ctx->contrast, ctx->green_gamma);
+		video->lookup_table[c + 256] = CALC(c, video->brightness, video->contrast, video->green_gamma);
 
 	for (c = 0; c < 256; c++)
-		ctx->lookup_table[c + 256 + 256] = CALC(c, ctx->brightness, ctx->contrast, ctx->blue_gamma);
+		video->lookup_table[c + 256 + 256] = CALC(c, video->brightness, video->contrast, video->blue_gamma);
 
 #undef CALC
 
