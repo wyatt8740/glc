50d5f7dcc3cf394eb87836ae73bd80932bdfe508
Pyry Haulos <pyry.haulos@gmail.com>
2008-03-18 22:18:01 +0200

    Use new format and types in core/info

--- src/glc/core/info.c
+++ src/glc/core/info.c
@@ -25,7 +25,7 @@
 
 #include "info.h"
 
-#define INFO_DETAILED_CTX           2
+#define INFO_DETAILED_VIDEO           2
 #define INFO_DETAILED_AUDIO_FORMAT  2
 #define INFO_FPS                    3
 #define INFO_AUDIO                  4
@@ -33,26 +33,27 @@
 #define INFO_PICTURE                5
 #define INFO_DETAILED_PICTURE       6
 
-struct info_ctx_s {
-	int created;
-	unsigned int w, h;
-	glc_ctx_i ctx_i;
+struct info_video_stream_s {
+	glc_stream_id_t id;
 	glc_flags_t flags;
+	glc_video_format_t format;
+	unsigned int w, h;
+
 	unsigned long pictures;
 	size_t bytes;
 
 	unsigned long fps;
 	glc_utime_t last_fps_time, fps_time;
 
-	struct info_ctx_s *next;
+	struct info_video_stream_s *next;
 };
 
-struct info_audio_s {
-	glc_audio_i audio_i;
+struct info_audio_stream_s {
+	glc_stream_id_t id;
 	unsigned long packets;
 	size_t bytes;
 
-	struct info_audio_s *next;
+	struct info_audio_stream_s *next;
 };
 
 struct info_s {
@@ -63,23 +64,23 @@ struct info_s {
 	glc_utime_t time;
 	int level;
 	FILE *stream;
-	glc_ctx_i prev_ctx;
 
-	struct info_ctx_s *ctx_list;
-	struct info_audio_s *audio_list;
+	struct info_video_stream_s *video_list;
+	struct info_audio_stream_s *audio_list;
 };
 
-int info_get_ctx(info_t info, struct info_ctx_s **ctx, glc_ctx_i ctx_i);
-int info_get_audio(info_t info, struct info_audio_s **audio,
-		   glc_audio_i audio_i);
+int info_get_video_stream(info_t info, struct info_video_stream_s **video,
+			  glc_stream_id_t id);
+int info_get_audio_stream(info_t info, struct info_audio_stream_s **audio,
+			  glc_stream_id_t id);
 
 void info_finish_callback(void *ptr, int err);
 int info_read_callback();
 
-void ctx_info(info_t info, glc_ctx_message_t *ctx_message);
-void pic_info(info_t info, glc_picture_header_t *pic_header);
-void audio_fmt_info(info_t info, glc_audio_format_message_t *fmt_message);
-void audio_info(info_t info, glc_audio_header_t *audio_header);
+void video_format_info(info_t info, glc_video_format_message_t *video_message);
+void video_data_info(info_t info, glc_video_data_header_t *pic_header);
+void audio_format_info(info_t info, glc_audio_format_message_t *fmt_message);
+void audio_data_info(info_t info, glc_audio_data_header_t *audio_header);
 void color_info(info_t info, glc_color_message_t *color_msg);
 
 void print_time(FILE *stream, glc_utime_t time);
@@ -91,7 +92,7 @@ int info_init(info_t *info, glc_t *glc)
 	memset(*info, 0, sizeof(struct info_s));
 
 	(*info)->glc = glc;
-	(*info)->ctx_list = NULL;
+	(*info)->video_list = NULL;
 	(*info)->time = 0;
 
 	(*info)->stream = stdout;
@@ -154,34 +155,34 @@ int info_process_wait(info_t info)
 void info_finish_callback(void *ptr, int err)
 {
 	info_t info = (info_t) ptr;
-	struct info_ctx_s *ctx;
-	struct info_audio_s *audio;
+	struct info_video_stream_s *video;
+	struct info_audio_stream_s *audio;
 
 	if (err)
 		glc_log(info->glc, GLC_ERROR, "info", "%s (%d)",
 			 strerror(err), err);
 
-	while (info->ctx_list != NULL) {
-		ctx = info->ctx_list;
-		info->ctx_list = info->ctx_list->next;
+	while (info->video_list != NULL) {
+		video = info->video_list;
+		info->video_list = info->video_list->next;
 
-		fprintf(info->stream, "video stream %d\n", ctx->ctx_i);
-		fprintf(info->stream, "  frames      = %lu\n", ctx->pictures);
+		fprintf(info->stream, "video stream %d\n", video->id);
+		fprintf(info->stream, "  frames      = %lu\n", video->pictures);
 		fprintf(info->stream, "  fps         = %04.2f\n",
-		       (double) (ctx->pictures * 1000000) / (double) (info->time));
+		       (double) (video->pictures * 1000000) / (double) (info->time));
 		fprintf(info->stream, "  bytes       = ");
-		print_bytes(info->stream, ctx->bytes);
+		print_bytes(info->stream, video->bytes);
 		fprintf(info->stream, "  bps         = ");
-		print_bytes(info->stream, (ctx->bytes * 1000000) / info->time);
+		print_bytes(info->stream, (video->bytes * 1000000) / info->time);
 
-		free(ctx);
+		free(video);
 	}
 
 	while (info->audio_list != NULL) {
 		audio = info->audio_list;
 		info->audio_list = info->audio_list->next;
 
-		fprintf(info->stream, "audio stream %d\n", audio->audio_i);
+		fprintf(info->stream, "audio stream %d\n", audio->id);
 		fprintf(info->stream, "  packets     = %lu\n", audio->packets);
 		fprintf(info->stream, "  pps         = %04.2f\n",
 		       (double) (audio->packets * 1000000) / (double) (info->time));
@@ -198,14 +199,14 @@ int info_read_callback(glc_thread_state_t *state)
 {
 	info_t info = (info_t) state->ptr;
 
-	if (state->header.type == GLC_MESSAGE_CTX)
-		ctx_info(info, (glc_ctx_message_t *) state->read_data);
-	else if (state->header.type == GLC_MESSAGE_PICTURE)
-		pic_info(info, (glc_picture_header_t *) state->read_data);
+	if (state->header.type == GLC_MESSAGE_VIDEO_FORMAT)
+		video_format_info(info, (glc_video_format_message_t *) state->read_data);
+	else if (state->header.type == GLC_MESSAGE_VIDEO_DATA)
+		video_data_info(info, (glc_video_data_header_t *) state->read_data);
 	else if (state->header.type == GLC_MESSAGE_AUDIO_FORMAT)
-		audio_fmt_info(info, (glc_audio_format_message_t *) state->read_data);
-	else if (state->header.type == GLC_MESSAGE_AUDIO)
-		audio_info(info, (glc_audio_header_t *) state->read_data);
+		audio_format_info(info, (glc_audio_format_message_t *) state->read_data);
+	else if (state->header.type == GLC_MESSAGE_AUDIO_DATA)
+		audio_data_info(info, (glc_audio_data_header_t *) state->read_data);
 	else if (state->header.type == GLC_MESSAGE_COLOR)
 		color_info(info, (glc_color_message_t *) state->read_data);
 	else if (state->header.type == GLC_MESSAGE_CLOSE) {
@@ -219,48 +220,48 @@ int info_read_callback(glc_thread_state_t *state)
 	return 0;
 }
 
-int info_get_ctx(info_t info, struct info_ctx_s **ctx, glc_ctx_i ctx_i)
+int info_get_video_stream(info_t info, struct info_video_stream_s **video, glc_stream_id_t id)
 {
-	struct info_ctx_s *fctx = info->ctx_list;
+	struct info_video_stream_s *fvideo = info->video_list;
 
-	while (fctx != NULL) {
-		if (fctx->ctx_i == ctx_i)
+	while (fvideo != NULL) {
+		if (fvideo->id == id)
 			break;
-		fctx = fctx->next;
+		fvideo = fvideo->next;
 	}
 
-	if (fctx == NULL) {
-		fctx = (struct info_ctx_s *) malloc(sizeof(struct info_ctx_s));
-		memset(fctx, 0, sizeof(struct info_ctx_s));
+	if (fvideo == NULL) {
+		fvideo = (struct info_video_stream_s *) malloc(sizeof(struct info_video_stream_s));
+		memset(fvideo, 0, sizeof(struct info_video_stream_s));
 
-		fctx->next = info->ctx_list;
-		info->ctx_list = fctx;
-		fctx->ctx_i = ctx_i;
+		fvideo->next = info->video_list;
+		info->video_list = fvideo;
+		fvideo->id = id;
 	}
 
-	*ctx = fctx;
+	*video = fvideo;
 
 	return 0;
 }
 
-int info_get_audio(info_t info, struct info_audio_s **audio,
-		   glc_audio_i audio_i)
+int info_get_audio_stream(info_t info, struct info_audio_stream_s **audio,
+		   glc_stream_id_t id)
 {
 	*audio = info->audio_list;
 
 	while (*audio != NULL) {
-		if ((*audio)->audio_i == audio_i)
+		if ((*audio)->id == id)
 			break;
 		*audio = (*audio)->next;
 	}
 
 	if (*audio == NULL) {
-		*audio = malloc(sizeof(struct info_audio_s));
-		memset(*audio, 0, sizeof(struct info_audio_s));
+		*audio = malloc(sizeof(struct info_audio_stream_s));
+		memset(*audio, 0, sizeof(struct info_audio_stream_s));
 
 		(*audio)->next = info->audio_list;
 		info->audio_list = *audio;
-		(*audio)->audio_i = audio_i;
+		(*audio)->id = id;
 	}
 
 	return 0;
@@ -275,160 +276,154 @@ int info_get_audio(info_t info, struct info_audio_s **audio,
 		__info_flag_op = " | "; \
 	}
 
-void ctx_info(info_t info, glc_ctx_message_t *ctx_message)
+void video_format_info(info_t info, glc_video_format_message_t *format_message)
 {
 	INFO_FLAGS
-	struct info_ctx_s *ctx;
-	info_get_ctx(info, &ctx, ctx_message->ctx);
+	struct info_video_stream_s *video;
+	info_get_video_stream(info, &video, format_message->id);
 
-	ctx->w = ctx_message->w;
-	ctx->h = ctx_message->h;
-	ctx->flags = ctx_message->flags;
-
-	if ((ctx_message->flags & GLC_CTX_UPDATE) && (!(ctx->created))) {
-		print_time(info->stream, info->time);
-		fprintf(info->stream, "error: GLC_CTX_UPDATE to uninitialized ctx %d\n",
-			ctx_message->ctx);
-	}
-
-	if ((ctx_message->flags & GLC_CTX_CREATE) && (ctx->created)) {
-		print_time(info->stream, info->time);
-		fprintf(info->stream, "error: GLC_CTX_CREATE to initalized ctx %d\n",
-			ctx_message->ctx);
-	}
-
-	if (ctx_message->flags & GLC_CTX_CREATE)
-		ctx->created = 1;
+	video->w = format_message->width;
+	video->h = format_message->height;
+	video->flags = format_message->flags;
+	video->format = format_message->format;
 
 	print_time(info->stream, info->time);
-	if (info->level >= INFO_DETAILED_CTX) {
+	if (info->level >= INFO_DETAILED_VIDEO) {
 		fprintf(info->stream, "video stream format message\n");
 
-		fprintf(info->stream, "  ctx         = %d\n", ctx_message->ctx);
+		fprintf(info->stream, "  stream id   = %d\n", format_message->id);
+		fprintf(info->stream, "  format      = ");
+		switch (video->format) {
+			case GLC_VIDEO_BGR:
+				fprintf(info->stream, "GLC_VIDEO_BGR\n");
+				break;
+			case GLC_VIDEO_BGRA:
+				fprintf(info->stream, "GLC_VIDEO_BGRA\n");
+				break;
+			case GLC_VIDEO_YCBCR_420JPEG:
+				fprintf(info->stream, "GLC_VIDEO_YCBCR_420JPEG\n");
+				break;
+			default:
+				fprintf(info->stream, "unknown format 0x%02x\n", video->format);
+		}
 		fprintf(info->stream, "  flags       = ");
-		INFO_FLAG(ctx_message->flags, GLC_CTX_CREATE)
-		INFO_FLAG(ctx_message->flags, GLC_CTX_UPDATE)
-		INFO_FLAG(ctx_message->flags, GLC_CTX_BGR)
-		INFO_FLAG(ctx_message->flags, GLC_CTX_BGRA)
-		INFO_FLAG(ctx_message->flags, GLC_CTX_YCBCR_420JPEG)
-		INFO_FLAG(ctx_message->flags, GLC_CTX_DWORD_ALIGNED)
+		INFO_FLAG(format_message->flags, GLC_VIDEO_DWORD_ALIGNED)
 		fprintf(info->stream, "\n");
-		fprintf(info->stream, "  width       = %u\n", ctx_message->w);
-		fprintf(info->stream, "  height      = %u\n", ctx_message->h);
+		fprintf(info->stream, "  width       = %u\n", format_message->width);
+		fprintf(info->stream, "  height      = %u\n", format_message->height);
 	} else
-		fprintf(info->stream, "video stream %d\n", ctx_message->ctx);
+		fprintf(info->stream, "video stream %d\n", format_message->id);
 }
 
-void pic_info(info_t info, glc_picture_header_t *pic_header)
+void video_data_info(info_t info, glc_video_data_header_t *pic_header)
 {
-	struct info_ctx_s *ctx;
-	info->time = pic_header->timestamp;
+	struct info_video_stream_s *video;
+	info->time = pic_header->time;
 
-	info_get_ctx(info, &ctx, pic_header->ctx);
-
-	if (!(ctx->created)) {
-		print_time(info->stream, info->time);
-		fprintf(info->stream, "error: picture to uninitialized ctx %d\n", ctx->ctx_i);
-	}
+	info_get_video_stream(info, &video, pic_header->id);
 
 	if (info->level >= INFO_DETAILED_PICTURE) {
 		print_time(info->stream, info->time);
 		fprintf(info->stream, "picture\n");
 
-		fprintf(info->stream, "  timestamp   = %lu\n", pic_header->timestamp);
-		fprintf(info->stream, "  ctx         = %d\n", pic_header->ctx);
-		fprintf(info->stream, "  size        = %ux%u\n", ctx->w, ctx->h);
+		fprintf(info->stream, "  stream id   = %d\n", pic_header->id);
+		fprintf(info->stream, "  time        = %lu\n", pic_header->time);
+		fprintf(info->stream, "  size        = %ux%u\n", video->w, video->h);
 	} else if (info->level >= INFO_PICTURE) {
 		print_time(info->stream, info->time);
-		fprintf(info->stream, "picture (ctx %d)\n", pic_header->ctx);
+		fprintf(info->stream, "picture (video %d)\n", pic_header->id);
 	}
 
-	if ((info->level >= INFO_DETAILED_PICTURE) && (info->prev_ctx != pic_header->ctx)) {
+	video->pictures++;
+	video->fps++;
+
+	if (video->format == GLC_VIDEO_BGR) {
+		video->bytes += video->w * video->h * 3;
+		if (video->flags & GLC_VIDEO_DWORD_ALIGNED)
+			video->bytes += video->h * (8 - (video->w * 3) % 8);
+	} else if (video->format == GLC_VIDEO_BGRA) {
+		video->bytes += video->w * video->h * 4;
+		if (video->flags & GLC_VIDEO_DWORD_ALIGNED)
+			video->bytes += video->h * (8 - (video->w * 4) % 8);
+	} else if (video->format == GLC_VIDEO_YCBCR_420JPEG)
+		video->bytes += (video->w * video->h * 3) / 2;
+
+	if ((info->level >= INFO_FPS) && (pic_header->time - video->fps_time >= 1000000)) {
 		print_time(info->stream, info->time);
-		fprintf(info->stream, "ctx switch from %d to %d\n", info->prev_ctx, pic_header->ctx);
+		fprintf(info->stream, "video %d: %04.2f fps\n", video->id,
+			(double) (video->fps * 1000000) / (double) (pic_header->time - video->last_fps_time));
+		video->last_fps_time = pic_header->time;
+		video->fps_time += 1000000;
+		video->fps = 0;
 	}
-
-	ctx->pictures++;
-	ctx->fps++;
-
-	if (ctx->flags & GLC_CTX_BGR) {
-		ctx->bytes += ctx->w * ctx->h * 3;
-		if (ctx->flags & GLC_CTX_DWORD_ALIGNED)
-			ctx->bytes += ctx->h * (8 - (ctx->w * 3) % 8);
-	} else if (ctx->flags & GLC_CTX_BGRA) {
-		ctx->bytes += ctx->w * ctx->h * 4;
-		if (ctx->flags & GLC_CTX_DWORD_ALIGNED)
-			ctx->bytes += ctx->h * (8 - (ctx->w * 4) % 8);
-	} else if (ctx->flags & GLC_CTX_YCBCR_420JPEG)
-		ctx->bytes += (ctx->w * ctx->h * 3) / 2;
-
-	if ((info->level >= INFO_FPS) && (pic_header->timestamp - ctx->fps_time >= 1000000)) {
-		print_time(info->stream, info->time);
-		fprintf(info->stream, "ctx %d: %04.2f fps\n", ctx->ctx_i,
-			(double) (ctx->fps * 1000000) / (double) (pic_header->timestamp - ctx->last_fps_time));
-		ctx->last_fps_time = pic_header->timestamp;
-		ctx->fps_time += 1000000;
-		ctx->fps = 0;
-	}
-
-	info->prev_ctx = pic_header->ctx;
 }
 
-void audio_fmt_info(info_t info, glc_audio_format_message_t *fmt_message)
+void audio_format_info(info_t info, glc_audio_format_message_t *fmt_message)
 {
 	INFO_FLAGS
 	print_time(info->stream, info->time);
 	if (info->level >= INFO_DETAILED_AUDIO_FORMAT) {
 		fprintf(info->stream, "audio stream format message\n");
 
-		fprintf(info->stream, "  stream      = %d\n", fmt_message->audio);
+		fprintf(info->stream, "  stream id   = %d\n", fmt_message->id);
+		fprintf(info->stream, "  format      = ");
+		switch (fmt_message->format) {
+			case GLC_AUDIO_S16_LE:
+				fprintf(info->stream, "GLC_AUDIO_S16_LE\n");
+				break;
+			case GLC_AUDIO_S24_LE:
+				fprintf(info->stream, "GLC_AUDIO_S24_LE\n");
+				break;
+			case GLC_AUDIO_S32_LE:
+				fprintf(info->stream, "GLC_AUDIO_S32_LE\n");
+				break;
+			default:
+				fprintf(info->stream, "unknown format 0x%02x\n",
+					fmt_message->format);
+		}
 		fprintf(info->stream, "  flags       = ");
 		INFO_FLAG(fmt_message->flags, GLC_AUDIO_INTERLEAVED)
-		INFO_FLAG(fmt_message->flags, GLC_AUDIO_FORMAT_UNKNOWN)
-		INFO_FLAG(fmt_message->flags, GLC_AUDIO_S16_LE)
-		INFO_FLAG(fmt_message->flags, GLC_AUDIO_S24_LE)
-		INFO_FLAG(fmt_message->flags, GLC_AUDIO_S32_LE)
 		fprintf(info->stream, "\n");
 		fprintf(info->stream, "  rate        = %d\n", fmt_message->rate);
 		fprintf(info->stream, "  channels    = %d\n", fmt_message->channels);
 	} else
-		fprintf(info->stream, "audio stream %d\n", fmt_message->audio);
+		fprintf(info->stream, "audio stream %d\n", fmt_message->id);
 }
 
-void audio_info(info_t info, glc_audio_header_t *audio_header)
+void audio_data_info(info_t info, glc_audio_data_header_t *audio_header)
 {
-	info->time = audio_header->timestamp;
-	struct info_audio_s *audio;
+	info->time = audio_header->time;
+	struct info_audio_stream_s *audio;
 
-	info_get_audio(info, &audio, audio_header->audio);
+	info_get_audio_stream(info, &audio, audio_header->id);
 	audio->packets++;
 	audio->bytes += audio_header->size;
 
 	if (info->level >= INFO_AUDIO_DETAILED) {
 		print_time(info->stream, info->time);
 		fprintf(info->stream, "audio packet\n");
-		fprintf(info->stream, "  stream      = %d\n", audio_header->audio);
-		fprintf(info->stream, "  timestamp   = %lu\n", audio_header->timestamp);
+		fprintf(info->stream, "  stream id   = %d\n", audio_header->id);
+		fprintf(info->stream, "  time        = %lu\n", audio_header->time);
 		fprintf(info->stream, "  size        = %ld\n", audio_header->size);
 	} else if (info->level >= INFO_AUDIO) {
 		print_time(info->stream, info->time);
-		fprintf(info->stream, "audio packet (stream %d)\n", audio_header->audio);
+		fprintf(info->stream, "audio packet (stream %d)\n", audio_header->id);
 	}
 }
 
 void color_info(info_t info, glc_color_message_t *color_msg)
 {
 	print_time(info->stream, info->time);
-	if (info->level >= INFO_DETAILED_CTX) {
+	if (info->level >= INFO_DETAILED_VIDEO) {
 		fprintf(info->stream, "color correction message\n");
-		fprintf(info->stream, "  ctx         = %d\n", color_msg->ctx);
+		fprintf(info->stream, "  stream id   = %d\n", color_msg->id);
 		fprintf(info->stream, "  brightness  = %f\n", color_msg->brightness);
 		fprintf(info->stream, "  contrast    = %f\n", color_msg->contrast);
 		fprintf(info->stream, "  red gamma   = %f\n", color_msg->red);
 		fprintf(info->stream, "  green gamma = %f\n", color_msg->green);
 		fprintf(info->stream, "  blue gamma  = %f\n", color_msg->blue);
 	} else
-		fprintf(info->stream, "color correction information for ctx %d\n", color_msg->ctx);
+		fprintf(info->stream, "color correction information for video %d\n", color_msg->id);
 }
 
 /*
