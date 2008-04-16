010395f2051e28b476cbf14d791f2a09e18a2eee
Pyry Haulos <pyry.haulos@gmail.com>
2008-04-16 18:43:00 +0300

    Improved command line agrument parsing in encode.sh

--- scripts/encode.sh
+++ scripts/encode.sh
@@ -1,44 +1,106 @@
 #!/bin/bash
 #
 # encode.sh -- encoding glc stream to x264-encoded video
-# Copyright (C) 2007 Pyry Haulos
+# Copyright (C) 2007-2008 Pyry Haulos
 # For conditions of distribution and use, see copyright notice in glc.h
 
-FILE="pid-*.glc"
+FILE=""
+QUALITY=""
+
 AUDIO="1"
-CTX="1"
+VIDEO="1"
+
 BITRATE="2000"
 QP="20"
 CRF="18"
+
 METHOD="qp"
-OFORMAT="mp4"
-OUT="video.${OFORMAT}"
+
+OUTFMT="mp4"
+OUT="video.${OUTFMT}"
+
 PASSLOG="pass.log"
 AUDIOTMP="audio.mp3.tmp"
-MULTIPASS="no"
-OPTS=""
 
-show-help () {
-	echo "$0 [file ${FILE}] [out (${OUT})] [ctx ${CTX}] [audio ${AUDIO}] [bitrate|crf|qp (${METHOD})] [value (${BITRATE}|${CRF}|${QP})] [additional options for mencoder]"
+MULTIPASS="no"
+ADDOPTS=""
+
+showhelp () {
+	echo "$0 [option]... [glc stream file]"
+	echo "  -o, --out=FILE       write video to FILE"
+	echo "                        default is ${OUT}"
+	echo "  -v, --video=NUM      video stream number"
+	echo "                        default is ${VIDEO}"
+	echo "  -a, --audio=NUM      audio stream number"
+	echo "                        default is ${AUDIO}"
+	echo "  -m, --method=METHOD  bitrate calculation method"
+	echo "                        supported methods are bitrate, qp, crf"
+	echo "                        default method is ${METHOD}"
+	echo "  -q, --quality=VAL    quality parameter"
+	echo "  -f, --outfmt=FORMAT  output container format"
+	echo "                        default is ${OUTFMT}"
+	echo "  -x, --addopts=OPTS   additional options to mencoder"
+	echo "  -h, --help           show this help"
 }
 
-if [ "$1" != "" -a "$1" != "-h" ]; then
-	FILE=$1
-else
-	if [ "$1" != "" ]; then
-		show-help
-		exit 1
-	fi
+OPT_TMP=`getopt -o o:v:a:m:q:f:x:h --long out:video:audio:method:quality:outfmt:addopts \
+	-n "$0" -- "$@"`
+if [ $? != 0 ]; then showhelp; exit 1; fi
+
+eval set -- "$OPT_TMP"
+
+while true; do
+	case "$1" in
+		-o|--out)
+			OUT="$2"
+			shift 2
+			;;
+		-v|--video)
+			VIDEO="$2"
+			shift 2
+			;;
+		-a|--audio)
+			AUDIO="$2"
+			shift 2
+			;;
+		-m|--method)
+			METHOD="$2"
+			shift 2
+			;;
+		-q|--quality)
+			QUALITY="$2"
+			shift 2
+			;;
+		-f|--outfmt)
+			OUTFMT="$2"
+			shift 2
+			;;
+		-a|--addopts)
+			ADDOPTS="$2"
+			shift 2
+			;;
+		-h|--help)
+			showhelp
+			exit 0
+			shift 2
+			;;
+		--)
+			shift
+			break
+			;;
+		*)
+			showhelp
+			exit 1
+			;;
+	esac
+done
+
+for arg do FILE=$arg; done
+if [ "$FILE" == "" ]; then
+	showhelp
+	exit 1
 fi
 
-[ "$2" != "" ] && OUT=$2
-[ "$3" != "" ] && CTX=$3
-[ "$4" != "" ] && AUDIO=$4
-[ "$5" != "" ] && METHOD=$5
-[ "$7" != "" ] && OPTS=$7
-
-#FPS=`glc-play "${FILE}" -s fps`
-#KEYINT=`expr "${FPS}" \* 10`
 KEYINT=300
 
 X264_OPTS="ref=4:mixed_refs:bframes=3:b_pyramid:bime:weightb:direct_pred=auto:filter=-1,0:partitions=all:turbo=1:threads=auto:keyint=${KEYINT}"
@@ -46,48 +108,52 @@ LAME_OPTS="q=4" # TODO configure q, cbr or abr
 
 case ${METHOD} in
 	crf)
-		[ "$6" != "" ] && BITRATE=$6 
+		[ "$QUALITY" != "" ] && CRF=$QUALITY
+		X264_OPTS="crf=${CRF}:${X264_OPTS}"
+		MULTIPASS="no"
+		;;
+	bitrate)
+		[ "$QUALITY" != "" ] && BITRATE=$QUALITY
 		X264_OPTS="bitrate=${BITRATE}:${X264_OPTS}"
-        MULTIPASS="yes"
+		MULTIPASS="yes"
 		;;
 	qp)
-		[ "$6" != "" ] && QP=$6
+		[ "$QUALITY" != "" ] && QP=$QUALITY
 		X264_OPTS="qp=${QP}:${X264_OPTS}"
 		MULTIPASS="yes"
 		;;
 	*)
-		show-help
+		showhelp
 		exit 1
 		;;
 esac
 
+[ "$OUTFMT" != "avi" ] && OUTFMT="lavf -lavfopts format=${OUTFMT}"
+
 glc-play "${FILE}" -o - -a "${AUDIO}" | lame -hV2 - "${AUDIOTMP}"
 
 if [ "${MULTIPASS}" == "no" ]; then
-	glc-play "${FILE}" -o - -y "${CTX}" | \
+	glc-play "${FILE}" -o - -y "${VIDEO}" | \
 		mencoder - \
 			-audiofile "${AUDIOTMP}"\
 			-demuxer y4m \
 			-ovc x264 \
 			-x264encopts "${X264_OPTS}" \
-			-oac copy \
-			${OPTS} \
-			-of lavf \
-			-lavfopts format="${OFORMAT}" \
+			-of ${OUTFMT} \
+			${ADDOPTS} \
 			-o "${OUT}"
 else
-	glc-play "${FILE}" -o - -y "${CTX}" | \
+	glc-play "${FILE}" -o - -y "${VIDEO}" | \
 		mencoder - \
 			-nosound \
 			-demuxer y4m \
 			-ovc x264 \
 			-x264encopts "${X264_OPTS}:pass=1" \
 			-passlogfile "${PASSLOG}" \
-			${OPTS} \
-			-of lavf \
-			-lavfopts format="${OFORMAT}" \
+			-of ${OUTFMT} \
+			${ADDOPTS} \
 			-o "${OUT}"
-	glc-play "${FILE}" -o - -y "${CTX}" | \
+	glc-play "${FILE}" -o - -y "${VIDEO}" | \
 		mencoder - \
 			-audiofile "${AUDIOTMP}" \
 			-demuxer y4m \
@@ -95,9 +161,8 @@ else
 			-x264encopts "${X264_OPTS}:pass=2" \
 			-passlogfile "${PASSLOG}" \
 			-oac copy \
-			${OPTS} \
-			-of lavf \
-			-lavfopts format="${OFORMAT}" \
+			-of ${OUTFMT} \
+			${ADDOPTS} \
 			-o "${OUT}"
 fi
 
