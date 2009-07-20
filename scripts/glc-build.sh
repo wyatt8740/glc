5a6e3bdf009751182fdfcf7821aec339a78079c1
Pyry Haulos <pyry.haulos@gmail.com>
2009-07-20 19:06:53 +0300

    Use "git clone" instead of "git-clone" in installer script.

--- scripts/glc-build.sh
+++ scripts/glc-build.sh
@@ -36,15 +36,14 @@ unpack () {
 }
 
 gitfetch () {
-	GIT_CLONE=`which git-clone 2> /dev/null`
-	GIT_PULL=`which git-pull 2> /dev/null`
+	GIT="`which git 2> /dev/null`"
 
 	if [ -d "$1" ]; then
 		cd "$1"
-		$GIT_PULL origin || die "Can't update $1"
+		$GIT pull origin || die "Can't update $1"
 		cd ..
 	else
-		$GIT_CLONE "git://nullkey.ath.cx/~pyry/$1" \
+		$GIT clone "git://nullkey.ath.cx/~pyry/$1" \
 			|| die "Can't clone $1"
 	fi
 }
@@ -114,15 +113,14 @@ ask-prompt
 read USE_GIT
 
 if [ "${USE_GIT}" == "y" ]; then
-	GIT_CLONE=`which git-clone 2> /dev/null`
-	if [ -x "${GIT_CLONE}" ]; then
+	if [ -x "`which git 2> /dev/null`" ]; then
 		gitfetch glc
 		gitfetch glc-support
 		gitfetch elfhacks
 		gitfetch packetstream
 		cd glc && ln -sf ../glc-support ./support && cd ..
 	else
-		die "git-clone not found (Ubuntu users: sudo apt-get install git-core)"
+		die "git not found (Ubuntu users: sudo apt-get install git-core)"
 	fi
 else
 	info "Fetching sources..."
